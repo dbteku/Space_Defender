96c8e8120a096bc3d4f4c994afe88d03e1447b45
Dallin Boyce <dboyce@student.neumont.edu>
2015-05-01 17:22:39 -0600

    Main.cpp

---
+++ Game/main.cpp
@@ -0,0 +1,81 @@
+#include "Ship.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int width = 1000;
+int height = 750;
+
+Ship ship;
+
+bool isAtEdge(Point p){
+	bool b = false;
+	if (p.x >= width){
+		b = p.x >= width;
+	}
+	else if (p.x <= width - width){
+		b = p.x <= width - width;
+	}
+
+	return b;
+
+}
+
+void checkKeyInput(){
+
+	if (isAtEdge(ship.getPos())){
+		ship.stop();
+	}
+	if (Core::Input::IsPressed(Core::Input::KEY_RIGHT)){
+		if (!isAtEdge(ship.getRightPos())){
+			if (ship.getRightPos().x <= width){
+				ship.goRight();
+			}
+		}
+		else{
+			if (ship.getRightPos().x <= width){
+				ship.goRight();
+			}
+			else{
+				ship.stop();
+			}
+		}
+	}
+	if (Core::Input::IsPressed(Core::Input::KEY_LEFT)){
+		if (!isAtEdge(ship.getLeftPos())){
+			if (ship.getLeftPos().x >= width - width){
+				ship.goLeft();
+			}
+		}
+		else{
+			if (ship.getLeftPos().x >= width - width){
+				ship.goLeft();
+			}
+			else{
+				ship.stop();
+			}
+		}
+	}
+}
+
+
+bool myUpdate(float dt)
+{
+	checkKeyInput();
+	ship.integrate();
+	return false;
+}
+
+void myDraw(Core::Graphics& graphics)
+{
+	ship.drawThyself(graphics);
+}
+
+void main()
+{
+	Core::Init("Space Defender", width, height);
+	Core::RegisterUpdateFn(myUpdate);
+	Core::RegisterDrawFn(myDraw);
+	Core::GameLoop();
+}
