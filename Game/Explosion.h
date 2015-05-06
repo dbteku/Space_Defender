ecc7e70348a1ded06974680d7c4cd3f7ed041cc6
Kurtis Lloyd <kurtyboyfloyd@gmail.com>
2015-05-06 17:54:56 -0600

    explosion Animation

--- Game/Explosion.h
+++ Game/Explosion.h
@@ -1,63 +1,11 @@
 #include "Point.h"
+#include "Counter.h"
 
 #ifndef EXPLOSION_H
 #define EXPLOSION_H
-
-
-class Explosion{
-	
-
-private:
-	Point topPoint;
-	Point botPoint;
-	Point leftPoint;
-	Point rightPoint;
-	Point topLeftPoint;
-	Point topRightPoint;
-	Point botLeftPoint;
-	Point botRightPoint;
-	Point base;
-	float spawnX = 500;
-	float spawnY = 700;
-
-public:
-	Explosion() :
-		topPoint(60,60),
-		botPoint(60,150),
-
-		leftPoint(20,100),
-		rightPoint(100,100),
-
-
-		topLeftPoint(90,130),
-		botRightPoint(33, 70),
-
-		topRightPoint(25,140),
-		botLeftPoint(90,60),
-		
-		base(spawnX,spawnY)
-
-	{}
-
-
-	void drawThySelf(Core::Graphics & g){
-		drawLine(g, base + topPoint, base + botPoint);
-		drawLine(g, base + leftPoint, base + rightPoint);
-		drawLine(g, base + topLeftPoint, base + botRightPoint);
-		drawLine(g, base + topRightPoint, base + botLeftPoint);
-		
-	}
-
-	void integrate()
-	{
-		base = base;
-	}
-
-};
-
 class  largeExplosion{
 
-	
+
 private:
 	Point topPoint;
 	Point botPoint;
@@ -73,7 +21,7 @@ private:
 
 public:
 	largeExplosion() :
-		topPoint(30,60),
+		topPoint(30, 60),
 		botPoint(90, 150),
 
 		leftPoint(0, 100),
@@ -157,5 +105,78 @@ public:
 
 
 };
+
+
+class Explosion{
+	largeExplosion lExplosion;
+	biggestExplosion bExplosion;
+	Counter count;
+	Counter count1;
+	Counter count2;
+	
+
+private:
+	int runCount = 100;
+	Point topPoint;
+	Point botPoint;
+	Point leftPoint;
+	Point rightPoint;
+	Point topLeftPoint;
+	Point topRightPoint;
+	Point botLeftPoint;
+	Point botRightPoint;
+	Point base;
+	float spawnX = 500;
+	float spawnY = 700;
+
+public:
+	Explosion() :
+		topPoint(60,60),
+		botPoint(60,150),
+
+		leftPoint(20,100),
+		rightPoint(100,100),
+
+
+		topLeftPoint(90,130),
+		botRightPoint(33, 70),
+
+		topRightPoint(25,140),
+		botLeftPoint(90,60),
+		
+		base(spawnX,spawnY)
+
+	{}
+
+
+	void drawThySelf(Core::Graphics & g){
+		drawLine(g, base + topPoint, base + botPoint);
+		drawLine(g, base + leftPoint, base + rightPoint);
+		drawLine(g, base + topLeftPoint, base + botRightPoint);
+		drawLine(g, base + topRightPoint, base + botLeftPoint);
+		
+	}
+
+	void integrate()
+	{
+		base = base;
+	}
+
+	void explosionAct(Core::Graphics& graphics){
+		if (count.explosionDelay()){
+			drawThySelf(graphics);
+			if (count1.explosionDelay()){
+				lExplosion.drawThySelf(graphics);
+				if (count2.explosionDelay()){
+					bExplosion.drawThySelf(graphics);
+				}
+			}
+		}
+		
+	}
+
+};
+
+
 #endif EXPLOSION_H
 
