1078a6b114f53e2db421a25d69e4d5645eeb767b
Dallin Boyce <dboyce@student.neumont.edu>
2015-05-06 21:41:03 -0600

    Main

--- Game/main.cpp
+++ Game/main.cpp
@@ -3,11 +3,7 @@ int width = 1000;
 int height = 750;
 
 Ship ship;
-Bullet bullet;
-EnemyManager manager;
-Explosion explo;
-biggestExplosion bExplo;
-largeExplosion lExplo;
+Manager manager;
 
 
 void update(){
@@ -15,30 +11,26 @@ void update(){
 }
 
 bool myUpdate(float dt){
-	checkKeyInput(ship, width, height);
-	ship.integrate();
-	explo.integrate();
-	bExplo.integrate();
-	lExplo.integrate();
-	update();
-	updateShips(manager,width,height);
-	isOutOfBounds(manager, width, height);
+	if (!ship.isDead()){
+		ship.integrate();
+		update();
+		update(manager, width, height);
+	}
 	return false;
 }
 
 void myDraw(Core::Graphics& graphics){
-	ship.drawThyself(graphics);
-	//explo.drawThySelf(graphics);
-	//lExplo.drawThySelf(graphics);
-	bExplo.drawThySelf(graphics);
-	
-
-	
-	vector<SmallShip>& ships = manager.getShips();
-	for (int x = 0; x < ships.size(); x++){
-		ships[x].drawThyself(graphics);
+	if (!ship.isDead()){
+		checkKeyInput(ship, width, height, graphics);
+		checkPlayerCollision(ship, manager.getShips());
+		checkBulletCollision(ship.getBullets(), manager.getShips());
+		ship.drawThyself(graphics);
+		vector<SmallShip>& ships = manager.getShips();
+		ship.act(width, height, graphics);
+		for (int x = 0; x < ships.size(); x++){
+			ships[x].act(graphics);
+		}
 	}
-	
 }
 
 void main(){
